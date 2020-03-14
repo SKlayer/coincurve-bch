a90560993ad44d5edf816a21784bccb439d7452c
sklayer <root@root.abc>
2020-03-14 13:59:02 +0800

    schnorr verify

--- freecrypto/keys.py
+++ freecrypto/keys.py
@@ -252,7 +252,7 @@ class PublicKey:
         if len(msg_hash) != 32:
             raise ValueError('Message hash must be 32 bytes long.')
         if schnorr:
-            verified = lib.secp256k1_schnorr_verify(self.context.ctx, der_to_cdata(signature, schnorr=True), msg_hash,
+            verified = lib.secp256k1_schnorr_verify(self.context.ctx, der_to_cdata(signature, schnorr=False), msg_hash,
                                                     self.public_key)
         else:
             verified = lib.secp256k1_ecdsa_verify(self.context.ctx, der_to_cdata(signature), msg_hash, self.public_key)
